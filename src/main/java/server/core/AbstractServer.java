7aa6e0da5a10f9c6ab5dae61a76f124a52e72e36
hbccdf <hbccdf@126.com>
2018-08-31 15:43:52 +0800

    add abstract server

--- src/main/java/server/core/AbstractServer.java
+++ src/main/java/server/core/AbstractServer.java
@@ -1,4 +1,45 @@
 package server.core;
 
-public class AbstractServer {
+public abstract class AbstractServer {
+    private int interval = 50;
+    private boolean running = true;
+
+    public AbstractServer() {
+        super();
+    }
+
+    public AbstractServer(int interval) {
+        super();
+        this.interval = interval;
+    }
+
+    public void startup(){
+        startup(true);
+    }
+
+    public void startup(boolean loop){
+        startup0();
+        if (loop) {
+            startLoop();
+        }
+    }
+
+    public void shutdown(){
+        running = false;
+    }
+
+    public abstract void onTick();
+
+    private void startLoop(){
+        running = true;
+        while(running){
+            try {
+                onTick();
+                Thread.sleep(interval);
+            } catch (InterruptedException e) {
+            }
+        }
+    }
+
+    public abstract void startup0();
 }
