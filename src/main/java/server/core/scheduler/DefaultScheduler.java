ca3ce3b0567c36128f1a31ea91e03ee19f6be6fe
hbccdf <hbccdf@126.com>
2018-08-31 11:23:17 +0800

    add scheduler

--- src/main/java/server/core/scheduler/DefaultScheduler.java
+++ src/main/java/server/core/scheduler/DefaultScheduler.java
@@ -1,4 +1,39 @@
 package server.core.scheduler;
 
-public class DefaultScheduler {
+import java.util.concurrent.*;
+
+public class DefaultScheduler implements IScheduler {
+    private ScheduledExecutorService threadPool = null;
+
+    public DefaultScheduler(int nThreads) {
+        threadPool = Executors.newScheduledThreadPool(nThreads);
+    }
+
+    @Override
+    public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
+        return threadPool.schedule(command, delay, unit);
+    }
+
+    @Override
+    public <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit) {
+        return threadPool.schedule(callable, delay, unit);
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleAtFixRate(Runnable command, long initialDelay, long period, TimeUnit unit) {
+        return threadPool.scheduleAtFixedRate(command, initialDelay, period, unit);
+    }
+
+    @Override
+    public ScheduledFuture<?> scheduleAtFixDelay(Runnable command, long initialDelay, long delay, TimeUnit unit) {
+        return threadPool.scheduleWithFixedDelay(command, initialDelay, delay, unit);
+    }
+
+    @Override
+    public void release() {
+        if (threadPool != null) {
+            threadPool.shutdown();
+            threadPool = null;
+        }
+    }
 }
--- src/main/java/server/core/scheduler/IScheduler.java
+++ src/main/java/server/core/scheduler/IScheduler.java
@@ -1,4 +1,17 @@
 package server.core.scheduler;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 public interface IScheduler {
+    ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit);
+
+    <V> ScheduledFuture<V> schedule(Callable<V> callable, long delay, TimeUnit unit);
+
+    ScheduledFuture<?> scheduleAtFixRate(Runnable command, long initialDelay, long period, TimeUnit unit);
+
+    ScheduledFuture<?> scheduleAtFixDelay(Runnable command, long initialDelay, long delay, TimeUnit unit);
+
+    void release();
 }
