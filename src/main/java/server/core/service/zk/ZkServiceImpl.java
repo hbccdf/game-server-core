808d77735235006372d339c043fc90065b1d13ae
hbccdf <hbccdf@126.com>
2018-11-28 12:01:08 +0800

    modify

--- src/main/java/server/core/service/zk/ZkServiceImpl.java
+++ src/main/java/server/core/service/zk/ZkServiceImpl.java
@@ -2,6 +2,7 @@ package server.core.service.zk;
 
 import com.google.inject.Singleton;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 import server.core.configuration.ConfigManager;
 import server.core.service.AbstractService;
@@ -17,7 +18,11 @@ public class ZkServiceImpl extends AbstractService implements IZkService {
     public boolean initialize() {
         try {
             ZkConfig config = ConfigManager.read(ZkConfig.class, "zookeeper");
-            zk = new ZooKeeper(config.getConnectString(), config.getSessionTimeout(), event -> log.info("{}:{}", event.getType(), event.getPath()));
+            zk = new ZooKeeper(config.getConnectString(), config.getSessionTimeout(), event -> {
+                if (event.getType() != Watcher.Event.EventType.None) {
+                    log.info("{}:{}", event.getType(), event.getPath());
+                }
+            });
             return true;
         } catch (Exception e) {
             log.error("", e);
--- src/main/java/server/core/util/StringUtil.java
+++ src/main/java/server/core/util/StringUtil.java
@@ -2,23 +2,7 @@ package server.core.util;
 
 public class StringUtil {
     public static boolean isMatch(String s, String p) {
-        boolean[][] dp = new boolean[s.length() + 1][p.length() + 1];
-        dp[0][0] = true;
-        for (int j = 0; j < p.length(); j++) {
-            if (dp[0][j] && p.charAt(j) == '*') {
-                dp[0][j + 1] = true;
-            }
-        }
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = 0; j < p.length(); j++) {
-                if (p.charAt(j) == '*') {
-                    dp[i + 1][j + 1] = dp[i][j + 1] || dp[i + 1][j];
-                } else if (p.charAt(j) == '?' || p.charAt(j) == s.charAt(i)) {
-                    dp[i + 1][j + 1] = dp[i][j];
-                }
-            }
-        }
-        return dp[s.length()][p.length()];
+        return s.matches(p);
     }
 
     public static boolean isNullOrEmpty(String string) {
